2433602971d8cdb293d3e96cf65e5b16c48de49b
Enoch <enochda@aol.com>
2018-06-19 23:13:30 -0400

    spacing

--- js/onload.js
+++ js/onload.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 function onReady(callback) {
-    var intervalID = window.setInterval(checkReady, 4000);
+    var intervalID = window.setInterval(checkReady, 6000);
 
     function checkReady() {
         if (document.getElementsByTagName('body')[0] !== undefined) {
