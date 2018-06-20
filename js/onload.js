77bb54358035d0b377762a4d043fb0d52714d5b3
Enoch <enochda@aol.com>
2018-06-19 23:46:19 -0400

    onlaod

--- js/onload.js
+++ js/onload.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 function onReady(callback) {
-    var intervalID = window.setInterval(checkReady, 6000);
+    var intervalID = window.setInterval(checkReady, 3000);
 
     function checkReady() {
         if (document.getElementsByTagName('body')[0] !== undefined) {
